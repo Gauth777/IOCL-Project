418f3e4d4652a17e784b09e0b3f856d5fa811e73
Naman <Allenbarr2609@gmail.com>
2026-06-15 14:41:54 +0530

    Update server.js

--- server.js
+++ server.js
@@ -28,9 +28,6 @@ function getMonthFromDate(dateString) {
 }
 const app = express();
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
 
 // Helper to read news from JSON database
 async function getNewsData() {
